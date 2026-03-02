fb5f029da475fd8206d37fa090ea73296a723fa1
koushik007 <koushikahamed2@gmail.com>
2026-03-02 07:59:46 +0600

    Delete shustho directory

--- shustho/js/popup.js
+++
@@ -1,8 +0,0 @@
-function openPopup(event) {
-    event.preventDefault();
-    document.getElementById('imagePopup').style.display = 'flex';
-  }
-  function closePopup() {
-    document.getElementById('imagePopup').style.display = 'none';
-  }
-  
